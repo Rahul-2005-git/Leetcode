f455a122026a9f566ca2263fbdd97cd0d73954cf
Rahul Solanke <solanker519@gmail.com>
2026-06-25 20:07:41 +0530

    Time: 1 ms (98.01%), Space: 47.2 MB (36.45%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,30 +1,30 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-       int n=intervals.length;
-       List <int []>res=new ArrayList<>();
-       int i=0;
-       while( i<n && intervals[i][1]<newInterval[0]){
-       res.add(intervals[i]);
-        i++;
+        List<int[]> res = new ArrayList<>();
+        int i = 0, n = intervals.length;
 
-   } while( i<n && intervals[i][0]<=newInterval[1]){
-        newInterval[0]=Math.min(newInterval[0],intervals[i][0]);
-        newInterval[1]=Math.max(newInterval[1],intervals[i][1]);
-        i++;
+        // 1. Add non-overlapping intervals before newInterval
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            res.add(intervals[i]);
+            i++;
+        }
 
-       }
-       res.add(newInterval);
-        while( i<n ){
+        // 2. Merge overlapping intervals
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
 
-       res.add(intervals[i]);
-        i++;
+        // 3. Add merged interval
+        res.add(newInterval);
 
-       }
+        // 4. Add remaining intervals
+        while (i < n) {
+            res.add(intervals[i]);
+            i++;
+        }
 
-       return res.toArray(new int[res.size()][]);
-       
-
-
-
+        return res.toArray(new int[res.size()][]);
     }
 }
