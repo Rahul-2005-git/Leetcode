d6779397b6dd5b815006affc072026f666a96ad0
Rahul Solanke <solanker519@gmail.com>
2026-07-20 23:51:18 +0530

    Time: 0 ms (100%), Space: 42 MB (52.28%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,6 +2,17 @@ class Solution {
     public int fib(int n) {
         if(n<1)return 0;
         if(n==1)return 1;
-        return fib(n-1)+fib(n-2);
+        int f=0;
+        int fn1=1,fn2=0;
+        int i=2;
+        // return fib(n-1)+fib(n-2);
+        while(i<=n){
+            f=fn1+fn2;
+            fn2=fn1;
+            fn1=f;
+            i++;
+        }
+        return f;
+
     }
 }
