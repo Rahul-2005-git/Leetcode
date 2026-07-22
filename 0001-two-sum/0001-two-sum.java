87a98fd577a0348e331d5edccb1132628b2541eb
Rahul Solanke <solanker519@gmail.com>
2026-07-22 19:30:04 +0530

    Time: 2 ms (99.3%), Space: 47.2 MB (23.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -18,15 +18,31 @@ class Solution {
         //     }
         // }
 
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(nums[i]+nums[j]==target){
+        //             res[0]=i;
+        //             res[1]=j;
+        //             return res;
+        //         }
+        //     }
+        // }
+
+
+        //by hashmap
+
+        Map<Integer,Integer> map=new HashMap<>();
+
         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    res[0]=i;
-                    res[1]=j;
-                    return res;
-                }
+            int complement =target -nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
             }
+            map.put(nums[i],i);
+
         }
+
+
         return res;
     }
 }
