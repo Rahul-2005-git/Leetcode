4015cc0259e9e446f335ef756bc2a33ea6de19f2
Rahul Solanke <solanker519@gmail.com>
2026-07-23 00:15:32 +0530

    Time: 22 ms (96.12%), Space: 48.9 MB (46.16%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -9,19 +9,24 @@ class Solution {
 
 
     // HashMap<Integer,Integer> map=new HashMap<>();
-    //    for(int i=0;i<nums.length;i++){
-    //     map.put(prefix[i],map.getOrDefault(prefix[i],0)+1);
-    //    }
+
+    // map.put(0,1);
 
     //    int ans=0;
     //     for(int i=0;i<nums.length;i++){
     //         if(map.containsKey(prefix[i]-k)){
     //             ans+=map.get(prefix[i]-k);
     //         }
+
+    //         map.put(prefix[i],map.getOrDefault(prefix[i],0)+1);
+
     //     }
     //     return ans;
 
-    HashMap<Integer, Integer> map = new HashMap<>();
+
+//compact version 
+
+HashMap<Integer, Integer> map = new HashMap<>();
 
 map.put(0, 1);
 
@@ -40,6 +45,7 @@ for (int num : nums) {
 }
 
 return ans;
+
 
     }
 }
