25f75510dce509cd4bbc7688d70c483f1cd28641
Rahul Solanke <solanker519@gmail.com>
2026-07-31 01:05:40 +0530

    Time: 8 ms (43.72%), Space: 47 MB (34.47%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        
+        HashMap<Character,Integer> map=new HashMap<>();
+
+        for(int i=0;i<ransomNote.length();i++){
+            map.put(ransomNote.charAt(i), map.getOrDefault(ransomNote.charAt(i),0)+1);
+        }
+
+        for(int i=0;i<magazine.length();i++){
+            if(map.containsKey(magazine.charAt(i))){
+                if(map.get(magazine.charAt(i))>1){
+                    map.put(magazine.charAt(i), map.get(magazine.charAt(i))-1);
+                }
+                else{
+
+                map.remove(magazine.charAt(i));
+                }
+            }
+        }
+
+        if(!map.isEmpty())return false;
+
+        return true;
+    }
+}
