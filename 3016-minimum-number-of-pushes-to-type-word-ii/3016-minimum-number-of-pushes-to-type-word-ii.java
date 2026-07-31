06a18174d396bca4c09b25d87abbba01814aa3b4
Rahul Solanke <solanker519@gmail.com>
2026-07-31 17:00:49 +0530

    Time: 9 ms (94.22%), Space: 47.9 MB (73.7%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minimumPushes(String word) {
+
+        int [] freq=new int[26];
+       
+        for(int i=0;i<word.length();i++){
+            freq[word.charAt(i)-'a']++;;
+        }
+
+        
+        Arrays.sort(freq);
+       
+        int ans=0;
+        for(int i=25;i>=0;i--){
+            if(i>17){
+                ans+=freq[i]*1;
+            }else if(i>9){
+                ans+=freq[i]*2;
+            }
+            else if(i>1){
+                ans+=freq[i]*3;
+            }
+            else{
+                ans+=freq[i]*4;
+            }
+        }
+        return ans;
+    }
+}
