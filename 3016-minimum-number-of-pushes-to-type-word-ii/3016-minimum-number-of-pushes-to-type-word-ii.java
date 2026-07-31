8571d15c3df443e38daf4070e25488dddeb40808
Rahul Solanke <solanker519@gmail.com>
2026-07-31 17:06:05 +0530

    Time: 8 ms (99.71%), Space: 47.8 MB (87.86%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -11,18 +11,27 @@ class Solution {
         Arrays.sort(freq);
        
         int ans=0;
+        int push=1;
+        int count=0;
         for(int i=25;i>=0;i--){
-            if(i>17){
-                ans+=freq[i]*1;
-            }else if(i>9){
-                ans+=freq[i]*2;
-            }
-            else if(i>1){
-                ans+=freq[i]*3;
-            }
-            else{
-                ans+=freq[i]*4;
-            }
+            // if(i>17){
+            //     ans+=freq[i]*1;
+            // }else if(i>9){
+            //     ans+=freq[i]*2;
+            // }
+            // else if(i>1){
+            //     ans+=freq[i]*3;
+            // }
+            // else{
+            //     ans+=freq[i]*4;
+            // }
+            if(freq[i]==0)break;
+        count++;
+        ans+=freq[i]*push;
+        if(count==8){
+            push++;
+            count=0;
+        }
         }
         return ans;
     }
