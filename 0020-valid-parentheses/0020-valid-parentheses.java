77ad0fb7cb0c5df3e9ee16d471840630d84a7aa2
Rahul Solanke <solanker519@gmail.com>
2026-08-02 18:29:17 +0530

    Time: 4 ms (36.82%), Space: 43.2 MB (58.22%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -5,23 +5,23 @@ class Solution {
         for(int i=0;i<s.length();i++){
             
 
-            if(s.charAt(i)=='('){stack.push('(');}
-            else if(s.charAt(i)=='{'){stack.push('{');}
-            else if(s.charAt(i)=='['){stack.push('[');}
-            else if(stack.isEmpty())return false;
+            if(s.charAt(i)=='('){stack.push(')');}
+            else if(s.charAt(i)=='{'){stack.push('}');}
+            else if(s.charAt(i)=='['){stack.push(']');}
+            else if(stack.isEmpty()  || stack.pop()!=s.charAt(i))return false;
 
-            else if(s.charAt(i)==')'){
-                if(stack.peek()=='(') stack.pop();
-                else return false;
-            }
-             else if(s.charAt(i)==']'){
-                if(stack.peek()=='[') stack.pop();
-                else return false;
-            }
-             else if(s.charAt(i)=='}'){
-                if(stack.peek()=='{') stack.pop();
-                else return false;
-            }
+            // else if(s.charAt(i)==')'){
+            //     if(stack.peek()=='(') stack.pop();
+            //     else return false;
+            // }
+            //  else if(s.charAt(i)==']'){
+            //     if(stack.peek()=='[') stack.pop();
+            //     else return false;
+            // }
+            //  else if(s.charAt(i)=='}'){
+            //     if(stack.peek()=='{') stack.pop();
+            //     else return false;
+            // }
         }
 
         if(stack.isEmpty())return true;
