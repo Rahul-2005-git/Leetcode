f8dd7d581a69410592dc1c9a37594d54618a1f56
Rahul Solanke <solanker519@gmail.com>
2026-08-12 01:59:32 +0530

    Time: 483 ms (12.98%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -8,8 +8,19 @@ round(
 --     select count(*) from queries q2 
 --     where q2.rating < 3 
 --     and q1.query_name=q2.query_name))
--- *100/count(q1.result)),2) 
-SUM(rating < 3) * 100.0 / COUNT(*),
+-- *100/count(q1.result)), 
+
+
+ SUM(
+            CASE
+                WHEN rating < 3 THEN 1
+                ELSE 0
+            END
+        ) * 100.0 / COUNT(query_name),
+
+
+
+-- SUM(rating < 3) * 100.0 / COUNT(*),
         2
         )
 as poor_query_percentage
