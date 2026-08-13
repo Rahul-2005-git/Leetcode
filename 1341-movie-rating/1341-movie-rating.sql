e1253242f1a1fda88a2bc130a4861c120565fa7e
Rahul Solanke <solanker519@gmail.com>
2026-08-13 19:31:09 +0530

    Time: 1359 ms (66.78%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,42 @@
+# Write your MySQL query statement below
+(
+    select t1.name as results from
+
+(select count(*) as count,u.user_id,u.name 
+
+from MovieRating mr
+left join Movies m
+on mr.movie_id=m.movie_id
+left join users u
+on mr.user_id=u.user_id
+
+group by u.user_id
+ ) t1
+ORDER BY t1.count DESC, t1.name ASC
+LIMIT 1
+)
+
+union all
+
+(
+select t2.title 
+from 
+(select avg(mr.rating) as avg,m.movie_id,m.title ,created_at
+
+from MovieRating mr
+left join Movies m
+on mr.movie_id=m.movie_id
+left join users u
+on mr.user_id=u.user_id
+-- where month(mr.created_at)=2 and year(mr.created)=2020
+where mr.created_at >= '2020-02-01' AND mr.created_at <= '2020-02-29'
+group by m.movie_id
+
+) t2
+
+order by t2.avg desc , t2.title asc
+LIMIT 1
+)
+;
+
+ 
