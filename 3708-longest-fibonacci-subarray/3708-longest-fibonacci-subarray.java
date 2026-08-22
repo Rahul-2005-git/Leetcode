6ec2fab997a3b128c3eedfe23b8a369d5cb3f7a1
Rahul Solanke <solanker519@gmail.com>
2026-08-22 11:35:55 +0530

    Time: 1 ms (100%), Space: 98.4 MB (13.7%) - LeetHub

---
+++ 3708-longest-fibonacci-subarray/3708-longest-fibonacci-subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        if(nums.length==1)return 1;
+        int maxLen=0;
+        int len=2;
+       for(int i=2;i<nums.length;i++){
+                if(nums[i]==nums[i-1]+nums[i-2]){
+                    len++;
+                }
+                else{
+                    len=2;
+                }
+                maxLen=Math.max(maxLen,len);
+       }
+       return maxLen;
+    }
+}
