7e2e59ba3e1e44c0d317c3b52ea1192f391a3d7c
Rahul Solanke <solanker519@gmail.com>
2026-08-28 13:39:47 +0530

    Time: 1 ms (100%), Space: 48.8 MB (71.44%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,44 +1,43 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // Ensure nums1 is the smaller array to optimize binary search range
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
         
-        int i=0,j=0;
-
-        int n1=nums1.length;
-        int n2=nums2.length;
-
-        int []merge =new int[n1+n2];
-
-        while(i!=n1 && j!=n2){
-            if(nums1[i]>nums2[j]){
-                merge[i+j]=nums2[j];
-                j++;
-            }
-            else{
-                merge[i+j]=nums1[i];
-                i++;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int low = 0;
+        int high = n1;
+        
+        while (low <= high) {
+            int cut1 = (low + high) / 2;
+            int cut2 = ((n1 + n2 + 1) / 2) - cut1;
+            
+            // Handle edge cases when partitions are empty
+            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+            int r1 = (cut1 == n1) ? Integer.MAX_VALUE : nums1[cut1];
+            int r2 = (cut2 == n2) ? Integer.MAX_VALUE : nums2[cut2];
+            
+            // Check if we found the correct partition
+            if (l1 <= r2 && l2 <= r1) {
+                if ((n1 + n2) % 2 == 1) {
+                    return Math.max(l1, l2);
+                } else {
+                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                }
+            } 
+            // Move left in nums1
+            else if (l1 > r2) {
+                high = cut1 - 1;
+            } 
+            // Move right in nums1
+            else {
+                low = cut1 + 1;
             }
         }
-        if(i==n1){
-            while(j<n2){
-
-            merge[i+j]=nums2[j];
-            j++;
-            }
-        }
-        else{
-            while(i<n1){
-                merge[i+j]=nums1[i];
-                i++;
-            }
-        }
-
-        double mid;
-        if((n1+n2)%2==1){
-            mid=merge[(n1+n2)/2];
-        } else{
-            mid=(merge[((n1+n2)/2)-1]+merge[(n1+n2)/2])/2.0;
-        }
-
-        return mid;
+        
+        return 0.0;
     }
 }
