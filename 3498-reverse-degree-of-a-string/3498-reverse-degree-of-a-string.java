a9d0a8f67b3997c0c36dd091755db5b4f4a7d985
Rahul Solanke <solanker519@gmail.com>
2026-09-20 18:58:55 +0530

    Time: 1 ms (100%), Space: 44 MB (67.44%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -3,10 +3,10 @@ class Solution {
         int product=0;
         for(int i=0;i<s.length();i++){
 
-            int num=s.charAt(i)-'a';
-            num=26-num;
+            // int num=26-s.charAt(i)-'a';
+            
             // System.out.println(num);
-            product+=num*(i+1);
+            product+=(26-(s.charAt(i)-'a'))*(i+1);
 
         }
         return product;
